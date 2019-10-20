1acdb0956bc1eade699831d3b732b847d1f09dfe
bgarcia95 <bgarcia.95@outlook.com>
2019-10-20 00:23:38 -0600

    Adding form and add product

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from "react";
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
 
+// Componentes
+import Header from "./componentes/Header";
+import NuevoProducto from "./componentes/NuevoProducto";
+import Productos from "./componentes/Productos";
+import EditarProducto from "./componentes/EditarProducto";
+
 function App() {
   return (
-    <Provider store={store}>
-      <p>Crud con redux</p>
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <Header />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Productos} />
+            <Route exact path="/productos/nuevo" component={NuevoProducto} />
+            <Route
+              exact
+              path="/productos/editar/:id"
+              component={EditarProducto}
+            />
+          </Switch>
+        </div>
+      </Provider>
+    </Router>
   );
 }
 
